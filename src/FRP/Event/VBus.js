8f1bd45d5ce1fa1da92ffb6c46e52c63fec91794
Mike Solomon <mike120982@gmail.com>
2022-06-07 14:45:37 +0300

    Fixes vbus

--- src/FRP/Event/VBus.js
+++ src/FRP/Event/VBus.js
@@ -6,50 +6,50 @@ export function unsafeMarkAsVbus(a) {
 }
 
 export function unsafeDestroyS(s) {
-    return () => {
-        for (const key in s) {
-            delete s[key];
-        }
-    };
+	return () => {
+		for (const key in s) {
+			delete s[key];
+		}
+	};
 }
 
 export function unsafePE(u) {
-    return () => {
-        const doAssigns = (s, p, e, u) => {
-            const ok = Object.keys(u);
-            for (var i = 0; i < ok.length; i++) {
-                if (
-                    u[ok[i]] instanceof Object &&
-                    u[ok[i]][_____$__$_$$_vbus] === _____$__$_$$_vbus
-                ) {
-                    const p0 = {};
-                    const e0 = {};
-                    doAssigns(s, p0, e0, u[ok[i]]);
-                    p[ok[i]] = p0;
-                    e[ok[i]] = e0;
-                } else {
-                    const rn = `${Math.random()}`;
-                    s[rn] = {};
-                    p[ok[i]] = (v) => () => {
-                        const rnk = Object.keys(s[rn]);
-                        for (var j = 0; j < rnk.length; j++) {
-                            s[rn][rnk[j]](v)();
-                        }
-                    };
-                    e[ok[i]] = (f) => () => {
-                        const k = `${Math.random()}`;
-                        s[rn][k] = f;
-                        return () => {
-                            delete s[rn][k];
-                        };
-                    };
-                }
-            }
-        };
-        const s = {};
-        const p = {};
-        const e = {};
-        doAssigns(s, p, e, u);
-        return { p: p, e: e, s: s };
-    };
+	return () => {
+		const doAssigns = (s, p, e, u) => {
+			const ok = Object.keys(u);
+			for (var i = 0; i < ok.length; i++) {
+				if (
+					u[ok[i]] instanceof Object &&
+					u[ok[i]][_____$__$_$$_vbus] === _____$__$_$$_vbus
+				) {
+					const p0 = {};
+					const e0 = {};
+					doAssigns(s, p0, e0, u[ok[i]]);
+					p[ok[i]] = p0;
+					e[ok[i]] = e0;
+				} else {
+					const rn = `${Math.random()}`;
+					s[rn] = {};
+					p[ok[i]] = (v) => () => {
+						const rnk = Object.values(s[rn]);
+						for (var j = 0; j < rnk.length; j++) {
+							rnk[j](v)();
+						}
+					};
+					e[ok[i]] = (f) => () => {
+						const k = `${Math.random()}`;
+						s[rn][k] = f;
+						return () => {
+							delete s[rn][k];
+						};
+					};
+				}
+			}
+		};
+		const s = {};
+		const p = {};
+		const e = {};
+		doAssigns(s, p, e, u);
+		return { p: p, e: e, s: s };
+	};
 }
