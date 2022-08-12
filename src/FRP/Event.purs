b384ff8943c5aa9b9b27363cc5ac6e95184e74c1
Mike Solomon <mike@meeshkan.com>
2022-08-12 13:41:57 +0300

    Adds burning function to complement hot (#11)
    * Adds burning
    
    * Adds docstring
    
    Co-authored-by: Mike Solomon <mike120982@gmail.com>

--- src/FRP/Event.purs
+++ src/FRP/Event.purs
@@ -20,6 +20,7 @@ module FRP.Event
   , memoize
   , MemoizeT
   , Memoize(..)
+  , burning
   , hot
   , HotT
   , Hot(..)
@@ -38,6 +39,7 @@ module FRP.Event
 
 import Prelude
 
+import Control.Alt ((<|>))
 import Control.Alternative (class Alt, class Alternative, class Plus)
 import Control.Apply (lift2)
 import Control.Monad.ST.Class (class MonadST, liftST)
@@ -348,6 +350,26 @@ type HotT =
   -> m { event :: AnEvent m a, unsubscribe :: m Unit }
 
 newtype Hot = Hot HotT
+
+-- | Makes an event _burning_ hot. Like hot, it will start firing immediately on left bind. In addition, it _always_ fires _immediately_ upon subscription with the most recent value.
+burning
+  :: forall m s a
+   . MonadST s m
+  => a
+  -> AnEvent m a
+  -> m { event :: AnEvent m a, unsubscribe :: m Unit }
+burning i e = do
+  r <- liftST $ Ref.new i
+  { event, push } <- create
+  unsubscribe <- subscribe e \x -> do
+    _ <- liftST $ Ref.write x r
+    push x
+  pure
+    { event: event <|> makeEvent \k -> do
+        liftST (Ref.read r) >>= k
+        pure (pure unit)
+    , unsubscribe
+    }
 
 --
 instance Action (Additive Int) (Event a) where
